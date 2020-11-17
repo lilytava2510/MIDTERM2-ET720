9bfe8b4b6d432d8b198009318f3122025b502bcf
lilytava2510 <lilytavares10@gmail.com>
2020-11-16 19:59:38 -0500

    MyResume added

---
+++ src/app/my-resume/my-resume.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-my-resume",
+  templateUrl: "./my-resume.component.html",
+  styleUrls: ["./my-resume.component.css"]
+})
+export class MyResumeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
