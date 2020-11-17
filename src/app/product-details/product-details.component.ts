d0e668e5c76403e9fcadaa3716034f642cdd2971
lilytava2510 <lilytavares10@gmail.com>
2020-11-16 20:36:47 -0500

    resume template

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -9,12 +9,11 @@ import { products } from "../products";
 })
 export class ProductDetailsComponent implements OnInit {
   product;
-  constructor() {}
-  private route: ActivatedRoute;
+  constructor(private route: ActivatedRoute) {}
 
- ngOnInit() {
-  this.route.paramMap.subscribe(params => {
-    this.product = products[+params.get('productId')];
-  });
-}
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.product = products[+params.get("productId")];
+    });
+  }
 }
